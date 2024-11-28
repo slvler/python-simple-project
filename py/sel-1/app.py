8c133ffd245fb58e451c7ccd97c3e744bae666ea
slvler <hselviler1@gmail.com>
2024-11-29 01:51:55 +0300

    update

---
+++ py/sel-1/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+CHROME_DRIVER_PATH = "/home/hicri/Masaüstü/py-101/py/sel-1/chromedriver"
+CHROME_BINARY_PATH = "/home/hicri/İndirilenler/chrome-linux64/chrome"
+
+options = Options()
+options.binary_location = CHROME_BINARY_PATH
+
+driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, options=options)
+
+driver.get("https://www.imdb.com")
+
+print("current_url", driver.current_url)
+print("title", driver.title)
+
+WAIT_ELEMENT = 3
+XPATH_VALUE = '//*[@id="imdbHeader"]'
+x = WebDriverWait(driver, WAIT_ELEMENT).until(EC.presence_of_element_located((By.XPATH, XPATH_VALUE)))
+outerHTML = x.get_attribute('outerHTML')
+print(outerHTML)
+
+# driver.close()
+# driver.quit()
